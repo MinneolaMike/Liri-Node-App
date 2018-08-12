36e15bddbd8441ef55f2a2d7822dee13e578a75b
chefgonzo <chefgonzo@gmail.com>
2018-08-12 10:17:35 -0400

    added the spotify keys

--- keys.js
+++ keys.js
@@ -1,12 +1,5 @@
-console.log('this is loaded');
+console.log('Keys are Loaded!!');
 
-// DotENV documentation
-const db = require('db')
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS
-})
 
 // Logins for Twitter but hidden in .env
 exports.twitter = {
